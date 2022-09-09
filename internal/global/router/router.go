9788df4bee8b970b9593a5d05ba18145e0a82c7a
Maheswara Devo <pundadevo21@gmail.com>
2022-09-09 17:19:02 +0800

    [#3] create new order finish

--- internal/global/router/router.go
+++ internal/global/router/router.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/maheswaradevo/hacktiv8-assignment2/internal/ping"
+
+	"github.com/maheswaradevo/hacktiv8-assignment2/internal/order"
 )
 
 func Init(router *mux.Router, db *sql.DB) {
@@ -13,4 +15,8 @@ func Init(router *mux.Router, db *sql.DB) {
 	pingService := ping.NewPingService()
 	pingController := ping.NewPingHandler(webRouter, pingService)
 	pingController.InitHandler()
+
+	orderService := order.ProvideOrderService(db)
+	orderController := order.ProvideOrderHandler(webRouter, orderService)
+	orderController.InitHandler()
 }
