eebcac10adc7e9d746c2ee5015883f111a9da5d5
Maheswara Devo <pundadevo21@gmail.com>
2022-09-13 22:42:33 +0800

    [#5]adding documentation using swaggo

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,11 +5,24 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	_ "github.com/maheswaradevo/hacktiv8-assignment2/docs"
 	"github.com/maheswaradevo/hacktiv8-assignment2/internal/global/config"
 	"github.com/maheswaradevo/hacktiv8-assignment2/internal/global/router"
 	"github.com/maheswaradevo/hacktiv8-assignment2/internal/global/server"
 	"github.com/maheswaradevo/hacktiv8-assignment2/pkg/database"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
+
+// @title Orders API
+// @version 1.0
+// @description This is service to managing order
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email soberkoder@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/license/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
 
 func main() {
 	log.Println("[main] starting server")
@@ -19,7 +32,7 @@ func main() {
 	db := database.GetDatabase()
 
 	router.Init(root, db)
-
+	root.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
 	s := server.ProvideServer(cfg.ServerAddress, root)
 	s.ListenAndServe()
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,21 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
+	github.com/swaggo/http-swagger v1.3.3
+	github.com/swaggo/swag v1.8.5
+)
+
+require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.20.0 // indirect
+	github.com/go-openapi/spec v0.20.7 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/swaggo/files v0.0.0-20220728132757-551d4a08d97a // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
